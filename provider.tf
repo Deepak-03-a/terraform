2e19328c4e350c44dba5dd169845f6c890ad7f2f
Deepak-03-a <kasavajahalaramdeepak@gmail.com>
2025-04-28 17:21:02 +0530

    creating a dynamic provider.tf file to and created variables to enter those values accordingly.

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-west-1"
+  access_key = var.access_key
+  secret_key = var.Secret_access_key
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable access_key {
+  description = "Please Enter the valid access key to perform the action"
+}
+
+variable Secret_access_key {
+  description = "Please Enter a valid secret access key to perform the action"
+}
